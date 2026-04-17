dc616ca7241c75d0d9364212640b2f21f32fbd52
Divya Pratap Singh <divya210205@gmail.com>
2026-04-17 08:52:51 +0530

    feat: add UserRepository with Spring Data JPA

---
+++ src/main/java/com/divyapratap/security/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.divyapratap.security.repository;
+
+import com.divyapratap.security.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
